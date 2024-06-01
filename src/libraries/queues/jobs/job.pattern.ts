95c10312cfdb5461d100377a13f1609abe537ae5
Anmol Gangwar <anmolgangwar64@gmail.com>
2024-06-01 14:33:57 +0530

    feat :zap: update job pattern and add queue constants

--- src/libraries/queues/jobs/job.pattern.ts
+++ src/libraries/queues/jobs/job.pattern.ts
@@ -5,19 +5,21 @@ export enum QueueEventJobPattern {
 
   // app queue job and app worker
   WELCOME_EMAIL = 'welcome_email',
-  CONNECTION_REQUEST_EMAIL = 'connection_request_email',
-  CONNECTION_REQUEST_ACCEPTANCE_EMAIL = 'connection_request_acceptance_email',
+  CONNECTION_REQUEST_RECEIVED = 'connection_request_received',
+  CONNECTION_REQUEST_ACCEPTED = 'connection_request_accepted',
 
   // app queue and content moderation worker service
-  POST_CREATION_EVENT = 'post_creation_event',
+  POST_CREATED = 'post_created',
 
-  // UNIVERSITY_NOTICE_NOTIFICATION queue and UNIVERSITY_NOTICE_NOTIFICATION_BULK for its children  job  university_notice notification worker
-  JOB_ALERT_EMAIL = 'job_alert_email',
-  JOB_ALERT_EMAIL_TO_USER = 'job_alert_email_to_user',
-  UNIVERSITY_NOTICE_EMAIL = 'university_notice_email',
-  UNIVERSITY_NOTICE_EMAIL_TO_USER = 'university_notice_email_to_user',
-  OFFLINE_EVENT_EMAIL = 'offline_event_email',
-  OFFLINE_EVENT_EMAIL_TO_USER = 'offline_event_email_to_user',
+  // UNIVERSITY_NOTIFICATION queue and UNIVERSITY_NOTIFICATION_BULK for its children  job  university_notice notification worker
+  JOB_CREATED = 'job_created',
+  JOB_CREATED_EMAIL_TO_USER = 'job_created_email_to_user',
+
+  UNIVERSITY_NOTICE_CREATED = 'university_notice_created',
+  UNIVERSITY_NOTICE_CREATED_EMAIL_TO_USER = 'university_notice_created_email_to_user',
+
+  EVENT_CREATED = 'event_created',
+  EVENT_CREATED_EMAIL_TO_USER = 'event_created_email_to_user',
 
   // USER_DYNAMIC_FEED_AND_CONNECTION_SUGGESTION_QUEUE and user_dynamic_feed_and_connection_suggestion_worker
   GENERATE_CONNECTIONS_SUGGESTION = 'generate_connections_suggestion',
--- src/libraries/queues/queue.constants.ts
+++ src/libraries/queues/queue.constants.ts
@@ -1,7 +1,7 @@
 export enum EmailQueues {
   AUTH_NOTIFICATION = 'email_auth_notification_queue',
   APP_NOTIFICATION = 'email_app_notification_queue',
-  UNIVERSITY_NOTICE_NOTIFICATION = 'email_university_notice_notification_queue',
+  UNIVERSITY_NOTIFICATION = 'email_university_notification_queue',
 
   USER_DYNAMIC_FEED_AND_CONNECTION_SUGGESTION_QUEUE = 'user_dynamic_feed_and_connection',
 }
